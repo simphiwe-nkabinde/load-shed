5cbcfc1220f1ab1d110d9fe3281c20d8969e7009
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-06-09 11:56:45 +0200

    change service methods to request from local server api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadsheddingStage, Province } from 'eskom-loadshedding-api';
+import { EskomApiService } from '../services/eskom-api.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(private eskomApiService: EskomApiService) {}
 
+  ngOnInit(): void {
+    this.eskomApiService.getCurrentStatus().subscribe(res => console.log(res), err => console.log(err))
+    this.eskomApiService.getMunicipalitiesByProvince(Province.GAUTENG).subscribe(res => console.log(res), err => console.log(err))
+    this.eskomApiService.searchSuburbs('benoni').subscribe(res => console.log(res), err => console.log(err))
+    this.eskomApiService.searchSuburbsInMunicipality(336, 'aard').subscribe(res => console.log(res), err => console.log(err))
+    this.eskomApiService.getSuburbSchedule(62648, LoadsheddingStage.STAGE_3).subscribe(res => console.log(res), err => console.log(err))
+    this.eskomApiService.getFullSuburbSchedule(62648).subscribe(res => console.log(res), err => console.log(err))
+  }
+  
 }
