77eac284a8055aeded7e8cf829ed9626b8546c9b
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-06-09 08:32:32 +0200

    create eskomApi service methods

--- src/app/services/eskom-api.service.ts
+++ src/app/services/eskom-api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Status, LoadsheddingStage } from 'eskom-loadshedding-api';
+import { Status, LoadsheddingStage, Search, Province, Municipality, 
+  Suburb, SearchSuburb, LoadsheddingSchedule, Schedule } from 'eskom-loadshedding-api';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,28 @@ export class EskomApiService {
 
   constructor() { }
 
+  getCurrentStatus(): Promise<LoadsheddingStage> {
+    return Status.getStatus()
+  }
+
+  getMunicipalitiesByProvince(province: Province): Promise<Municipality[]> {
+    return Search.getMunicipalities(province)
+  }
+
+  searchSuburbs(searchTerm:string): Promise<SearchSuburb[]> {
+    return Search.searchSuburbs(searchTerm);
+  }
+
+  searchSuburbsInMunicipality(municipalityId: number, searchTerm?: string): Promise<Suburb[]> {
+    return Search.getMunicipalitySuburbs(municipalityId, searchTerm)
+  }
+
+  getSuburbSchedule(suburbId: number, loadsheddingStage: LoadsheddingStage): Promise<LoadsheddingSchedule> {
+    return Schedule.getSchedule(suburbId, loadsheddingStage)
+  }
+
+  getFullSuburbSchedule(suburbId: number): Promise<LoadsheddingSchedule[]> {
+    return Schedule.getFullSchedule(suburbId)
+  }
 
 }
