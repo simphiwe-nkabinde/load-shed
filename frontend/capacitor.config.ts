c36bdf8b36e88238270b3e65780c44f8ad008230
Monoametsi <216026633@student.uj.ac.za>
2022-06-14 16:18:59 +0200

    make it better

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.load-shed',
   appName: 'load-shed',
   webDir: 'www',
   bundledWebRuntime: false
